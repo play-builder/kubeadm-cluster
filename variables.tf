bbd35c8b28d7b4d504075049855f13b05c500b15
play-builder <playbuilder47@gmail.com>
2024-12-17 10:00:03 +0900

    feat: Add instance type and networking variables

--- variables.tf
+++ variables.tf
@@ -21,3 +21,26 @@ variable "public_subnet_cidr" {
   type        = string
   default     = "10.0.1.0/24"
 }
+
+variable "availability_zone" {
+  description = "Availability zone for the subnet"
+  type        = string
+  default     = "ap-northeast-2a"
+}
+
+variable "control_plane_instance_type" {
+  description = "EC2 instance type for the control plane node"
+  type        = string
+  default     = "t3.medium" # 2 vCPU, 4GB RAM
+}
+
+variable "worker_instance_type" {
+  description = "EC2 instance type for worker nodes"
+  type        = string
+  default     = "t3.small" # 2 vCPU, 2GB RAM
+}
+
+variable "worker_count" {
+  description = "Number of worker nodes to create"
+  type        = number
+  default     = 2
