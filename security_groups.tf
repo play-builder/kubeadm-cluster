2c87058aca15f461d7f1d90b51b7a4cddc82e895
play-builder <playbuilder47@gmail.com>
2024-12-21 10:36:11 +0900

    infra(sg): Finalize cross-group rules for pod network

--- security_groups.tf
+++ security_groups.tf
@@ -84,3 +84,47 @@ resource "aws_security_group" "worker" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "wk_kubelet" {
+  security_group_id = aws_security_group.worker.id
+  description       = "kubelet API from VPC"
+  from_port         = 10250
+  to_port           = 10250
+  ip_protocol       = "tcp"
+  cidr_ipv4         = var.vpc_cidr
+}
+
+resource "aws_vpc_security_group_ingress_rule" "wk_nodeport" {
+  security_group_id = aws_security_group.worker.id
+  description       = "NodePort services (learning, open to public)"
+  from_port         = 30000
+  to_port           = 32767
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "wk_inter_worker" {
+  security_group_id            = aws_security_group.worker.id
+  description                  = "Inter-worker pod network (CNI overlay)"
+  ip_protocol                  = "-1"
+  referenced_security_group_id = aws_security_group.worker.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "wk_from_cp" {
+  security_group_id            = aws_security_group.worker.id
+  description                  = "All traffic from control plane"
+  ip_protocol                  = "-1"
+  referenced_security_group_id = aws_security_group.control_plane.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "cp_from_wk" {
+  security_group_id            = aws_security_group.control_plane.id
+  description                  = "All traffic from workers (pod network)"
+  ip_protocol                  = "-1"
+  referenced_security_group_id = aws_security_group.worker.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "wk_all_outbound" {
+  security_group_id = aws_security_group.worker.id
+  description       = "Allow all outbound (apt, image pull, SSM)"
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+}
