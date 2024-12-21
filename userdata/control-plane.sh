0b0e73146b73e310c4b7d3ebe92c8d4e4d067dea
play-builder <playbuilder47@gmail.com>
2024-12-21 21:02:54 +0900

    feat(userdata): Configure kubectl for ubuntu user

--- userdata/control-plane.sh
+++ userdata/control-plane.sh
@@ -13,3 +13,19 @@ CONTROL_PLANE_IP="${control_plane_ip}"
 POD_NETWORK_CIDR="${pod_network_cidr}"
 
 # Install common packages (containerd, kubeadm, kubelet, kubectl)
+${common_script}
+
+# Run kubeadm init
+echo "=== kubeadm init started at $(date) ==="
+
+kubeadm init \
+  --apiserver-advertise-address="$CONTROL_PLANE_IP" \
+  --pod-network-cidr="$POD_NETWORK_CIDR" \
+  --node-name="$NODE_HOSTNAME" \
+  --kubernetes-version="stable-$KUBERNETES_VERSION" \
+  --upload-certs \
+  2>&1 | tee /var/log/kubeadm-init.log
+
+# Configure kubectl for root user
+mkdir -p /root/.kube
+cp /etc/kubernetes/admin.conf /root/.kube/config
