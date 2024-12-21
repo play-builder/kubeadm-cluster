6979130cdbb16923132013ed4d62c6f1cc61ff1f
play-builder <playbuilder47@gmail.com>
2024-12-21 21:49:16 +0900

    infra(iam): Create SSM IAM role

---
+++ iam.tf
@@ -0,0 +1,10 @@
+resource "aws_iam_role" "ssm_role" {
+  name = "${var.cluster_name}-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
