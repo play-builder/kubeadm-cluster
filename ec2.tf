80927b792e492bcbdd69255e4b50ae77e50316e4
play-builder <playbuilder47@gmail.com>
2024-12-21 10:07:22 +0900

    feat(ec2): Finalize lifecycle rules and templatefile injection

--- ec2.tf
+++ ec2.tf
@@ -60,3 +60,35 @@ resource "aws_spot_instance_request" "workers" {
   count = var.worker_count
 
   ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.worker_instance_type
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.worker.id]
+  iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
+
+  associate_public_ip_address = true
+  source_dest_check           = false
+
+  spot_type            = "one-time"
+  wait_for_fulfillment = true
+
+  root_block_device {
+    volume_size           = var.ebs_volume_size
+    volume_type           = "gp3"
+    delete_on_termination = true
+  }
+
+  user_data = templatefile("${path.module}/userdata/worker.sh", {
+    node_hostname      = "${var.cluster_name}-worker-${count.index + 1}"
+    kubernetes_version = var.kubernetes_version
+    common_script      = local.common_script
+  })
+
+  lifecycle {
+    ignore_changes = [user_data, ami]
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-worker-${count.index + 1}"
+    Role = "worker"
+  }
+}
