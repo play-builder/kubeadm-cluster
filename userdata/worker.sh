951c984f1b77fb2d5e6199892ca5f450d8b87bb2
play-builder <playbuilder47@gmail.com>
2024-12-21 09:59:12 +0900

    feat(userdata): Inject common script via templatefile

--- userdata/worker.sh
+++ userdata/worker.sh
@@ -3,3 +3,8 @@
 set -euxo pipefail
 
 exec > >(tee /var/log/kubeadm-bootstrap.log) 2>&1
+
+# Variables injected by Terraform templatefile
+export NODE_HOSTNAME="${node_hostname}"
+export KUBERNETES_VERSION="${kubernetes_version}"
+
